bb014ad0b1705cc5e21a9d3dcc63e5fc2d72f992
MRazin172 <muzzammil.razin@msam.christuniversity.in>
2023-10-06 10:31:49 +0530

    commit12

--- ADT/settings.py
+++ ADT/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.urls.path',
+    'django.urls.include'
 ]
 
 MIDDLEWARE = [
--- ADT/urls.py
+++ ADT/urls.py
@@ -1,5 +1,6 @@
 """
-URL configuration for ADT project.
+URL configuration for ADT project.from django.urls import path, include
+
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +16,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('<RNM>.urls')),
 ]
